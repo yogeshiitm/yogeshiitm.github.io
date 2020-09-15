b23822304142229299785463440ad3ff2b683007
Yogesh Agarwala <yogesh@smail.iitm.ac.in>
2020-09-15 17:02:11 +0530

    Add files via upload

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,4 +1,4 @@
-<?php
+server is<?php
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
