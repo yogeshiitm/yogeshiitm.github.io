559589f14a7d1be113a4b39f6436546bc428a4c0
Yogesh Agarwala <yogesh@smail.iitm.ac.in>
2020-09-23 02:12:16 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,16 @@
-(function($) {
-"use strict"; // Start of use strict
-
-// Smooth scrolling using jQuery easing
-$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: (target.offset().top - 71)
-      }, 1000, "easeInOutExpo");
-      return false;
-    }
-  }
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
 });
-
-// Scroll to top button appear
-$(document).scroll(function() {
-  var scrollDistance = $(this).scrollTop();
-  if (scrollDistance > 100) {
-    $('.scroll-to-top').fadeIn();
-  } else {
-    $('.scroll-to-top').fadeOut();
-  }
-});
-
-// Closes responsive menu when a scroll trigger link is clicked
-$('.js-scroll-trigger').click(function() {
-  $('.navbar-collapse').collapse('hide');
-});
-
-// Activate scrollspy to add active class to navbar items on scroll
-$('body').scrollspy({
-  target: '#mainNav',
-  offset: 80
-});
-
-// Collapse Navbar
-var navbarCollapse = function() {
-  if ($("#mainNav").offset().top > 100) {
-    $("#mainNav").addClass("navbar-shrink");
-  } else {
-    $("#mainNav").removeClass("navbar-shrink");
-  }
-};
-// Collapse now if page is not at top
-navbarCollapse();
-// Collapse the navbar when page is scrolled
-$(window).scroll(navbarCollapse);
-
-// Floating label headings for the contact form
-$(function() {
-  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-    $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-  }).on("focus", ".floating-label-form-group", function() {
-    $(this).addClass("floating-label-form-group-with-focus");
-  }).on("blur", ".floating-label-form-group", function() {
-    $(this).removeClass("floating-label-form-group-with-focus");
-  });
-});
-
-})(jQuery); // End of use strict
-  
